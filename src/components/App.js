edb49b911e7e506b10e0088ac73bae9dd9e9ecbf
Daniel Pickett <danielpickett@Daniels-Air.attlocal.net>
2022-09-14 06:54:10 -0400

    24. Orderbook

--- src/components/App.js
+++ src/components/App.js
@@ -8,6 +8,7 @@ import {
   loadAccount,
   loadTokens,
   loadExchange,
+  loadAllOrders,
   subscribeToEvents
 } from '../store/interactions';
 
@@ -15,6 +16,7 @@ import Navbar from './Navbar'
 import Markets from './Markets'
 import Balance from './Balance'
 import Order from './Order'
+import OrderBook from './OrderBook'
 
 function App() {
   const dispatch = useDispatch()
@@ -41,6 +43,9 @@ function App() {
  
     const exchangeConfig = config[chainId].exchange
     const exchange = await loadExchange(provider, exchangeConfig.address, dispatch)
+
+    loadAllOrders(provider, exchange, dispatch)
+
 
     subscribeToEvents(exchange, dispatch)
   }
@@ -72,7 +77,7 @@ function App() {
 
           {/* Trades */}
 
-          {/* OrderBook */}
+          <OrderBook />
 
         </section>
       </main>
